b913d6efb59a3e2062741d0bbd2ca30c4e71e8a0
coroner <linayn9@gmail.com>
2020-10-22 16:34:13 +0300

    n04 - signature of the task

--- n04 - practice12/src/Main.java
+++ n04 - practice12/src/Main.java
@@ -1,4 +1,8 @@
 public class Main {
+    /* Вариант 4 - Владельцы автомобилей
+    Доп. операции: 1. Список автомобилей, числящихся в угоне
+    2. Установить факт угона автомобиля с заданным номером
+     */
     public static void main(String[] args) {
         ByteFile f = new ByteFile("pr12.txt", "test.txt");
         f.createNewBinary();
